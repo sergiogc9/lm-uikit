0ba737578a424bf2878ef143794836b010329961
Sergio Gómez <sergi.gomez@launchmetrics.com>
2020-06-12 13:52:27 +0200

    First test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { TextField } from './lib';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <TextField />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/lib/TextField/TextField.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './TextField.css';
+
+const TextField: React.FC = props => {
+  return <div>Text field!</div>;
+}
+
+export default TextField;
